67ddf59391a16083d3ec0098418778a8f3fcb5a6
kurironight <titechcycsin17@gmail.com>
2021-02-27 16:11:17 +0900

    env上にて# edge_indicesでは触れられていないノードが存在しないようにする

--- env/gym_barfem.py
+++ env/gym_barfem.py
@@ -12,6 +12,22 @@ class BarFemGym(MetamechGym):
 
     def calculate_simulation(self):
         nodes_pos, edges_indices, edges_thickness, _ = self.extract_node_edge_info()
+        # edge_indicesでは触れられていないノードが存在しないようにする
+        node_num = nodes_pos.shape[0]
+        assert node_num >= np.max(
+            edges_indices), 'edges_indicesに，ノード数以上のindexを示しているものが発生'
+        mask = np.isin(np.arange(node_num), edges_indices)
+        if not np.all(mask):
+            processed_edges_indices = edges_indices.copy()
+            prior_index = np.arange(node_num)[mask]
+            processed_nodes_pos = nodes_pos[mask]
+            for index, prior_index in enumerate(prior_index):
+                if index != prior_index:
+                    processed_edges_indices[edges_indices ==
+                                            prior_index] = index
+            nodes_pos = processed_nodes_pos
+            edges_indices = processed_edges_indices
+
         displacement = barfem(nodes_pos, edges_indices, edges_thickness, self.input_nodes,
                               self.input_vectors, self.frozen_nodes)
 
