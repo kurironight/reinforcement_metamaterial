48561186b75c17572db2f04dbbb5743037193d12
kurironight <titechcycsin17@gmail.com>
2021-03-14 14:37:43 +0900

    torch.tensorによってbackwardが消えることの対応

--- confirm/step1/actor_critic.py
+++ confirm/step1/actor_critic.py
@@ -17,15 +17,20 @@ Saved_Action = namedtuple('SavedAction', ['action', 'value'])
 
 
 class Edgethick_Actor(ActorNetwork):
-    def __init__(self, emb_size=300):
+    def __init__(self, emb_size=300, hidden2_size=300):
         super(Edgethick_Actor, self).__init__(num_state=emb_size, num_action=2,
-                                              hidden1_size=400, hidden2_size=300, init_w=3e-3)
+                                              hidden1_size=400, hidden2_size=hidden2_size, init_w=3e-3)
+        self.fc3 = nn.Linear(hidden2_size, 1)
+        self.fc4 = nn.Linear(hidden2_size, 1)
         self.saved_actions = []
 
     def forward(self, h):
         h = F.relu(self.fc1(h))
         h = F.relu(self.fc2(h))
-        y = 0.1+0.9*torch.sigmoid(self.fc3(h))  # var to mean todekaeru
+        mean = torch.sigmoid(self.fc3(h))  # var to mean todekaeru
+        std = 0.1*torch.sigmoid(self.fc4(h))
+        y = torch.cat([mean, std], dim=1)
+        print(y)
         return y
 
 
@@ -84,10 +89,11 @@ def finish_episode(Critic, Edge_thickness, Critic_opt, Edge_thick_opt, gamma):
         returns.insert(0, R)
     returns = torch.tensor(returns)
 
-    for (action, value),  (edge_thick_mean, edge_thick_var),  R in zip(GCN_saved_actions, Edge_thickness_saved_actions, returns):
+    for (action, value),  (edge_thick_mean, edge_thick_std),  R in zip(GCN_saved_actions, Edge_thickness_saved_actions, returns):
 
         advantage = R - value.item()
         node_num = 2
+        print("advantage:", advantage)
 
         # calculate actor (policy) loss
         if action["end"]:
@@ -99,15 +105,18 @@ def finish_episode(Critic, Edge_thickness, Critic_opt, Edge_thick_opt, gamma):
                     print("okasii")
                 else:
                     edge_thick_mean_loss = F.l1_loss(torch.from_numpy(
-                        action["edge_thickness"]).double(), torch.tensor([edge_thick_mean]).double())
+                        action["edge_thickness"]).double(), edge_thick_mean.reshape((1)).double())
                     edge_thick_var_loss = F.l1_loss(torch.from_numpy(
-                        np.abs(action["edge_thickness"]-edge_thick_mean.item())).double(), torch.tensor([torch.sqrt(edge_thick_var)]).double())
+                        np.abs(action["edge_thickness"]-edge_thick_mean.item())).double(), edge_thick_std.reshape((1)).double())
                     policy_losses.append(
                         (edge_thick_mean_loss+edge_thick_var_loss) * advantage)
 
         # calculate critic (value) loss using L1 loss
         value_losses.append(
             F.l1_loss(value.double(), torch.tensor([[R]]).double()))
+
+    #print("policy_losses:", policy_losses)
+    #print("value_losses:", value_losses)
 
     # reset gradients
     Critic_opt.zero_grad()
