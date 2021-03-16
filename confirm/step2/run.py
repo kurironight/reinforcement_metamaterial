f43ab59a1d925a27e7da2f6425627cece4666c4a
kurironight <titechcycsin17@gmail.com>
2021-03-16 21:21:42 +0900

    actor-criticのコメント修正

--- confirm/step2/run.py
+++ confirm/step2/run.py
@@ -12,7 +12,7 @@ import pickle
 
 def actor_critic():
     """Actor-Criticを行う．学習はDL（GCN以外）で
-    Actorの指定できる幅は0.1-1となっている"""
+    Actorの指定できるものは，ノード1とノード2であり，一つのエッジのみを選択できる．"""
 
     max_episodes = 5000
     test_name = "5000"  # 実験名
@@ -72,91 +72,3 @@ def actor_critic():
     env.close()
     plot_efficiency_history(history, os.path.join(
         log_dir, 'learning_effi_curve.png'))
-
-
-"""
-
-
-
-
-def actor_critic_mean():
-
-    test_num = 5
-
-    max_episodes = 5000
-    test_name = "5timess_with_std"  # 実験名
-
-    log_dir = "confirm/step2/ac_results/{}".format(test_name)
-    assert not os.path.exists(log_dir), "already folder exists"
-    os.makedirs(log_dir, exist_ok=True)
-
-    history = {}
-    for i in range(test_num):
-        history["{}".format(i)] = {}
-        history["{}".format(i)]['epoch'] = []
-        history["{}".format(i)]['result_efficiency'] = []
-
-        node_pos, input_nodes, input_vectors,\
-            output_nodes, output_vectors, frozen_nodes,\
-            edges_indices, edges_thickness, frozen_nodes = easy_dev()
-        env = Step1Gym(node_pos, input_nodes, input_vectors,
-                       output_nodes, output_vectors, frozen_nodes,
-                       edges_indices, edges_thickness, frozen_nodes)
-        env.reset()
-
-        max_steps = 1
-        lr_actor = 1e-4
-        lr_critic = 1e-3
-        weight_decay = 1e-2
-        gamma = 0.99
-
-        device = torch.device('cpu')
-
-        actorNet = Edgethick_Actor().to(device)
-        criticNet = Edgethick_Critic().to(device)
-        optimizer_actor = optim.Adam(actorNet.parameters(), lr=lr_actor)
-        optimizer_critic = optim.Adam(
-            criticNet.parameters(), lr=lr_critic, weight_decay=weight_decay)
-
-        for episode in range(max_episodes):
-            observation = env.reset()
-            observation = np.array([0, 1])
-
-            for step in range(max_steps):
-                action = select_action(
-                    observation, actorNet, criticNet, device)
-
-                next_observation, _, done, _ = env.step(action)
-                reward = env.calculate_simulation(mode='force')
-                criticNet.rewards.append(reward)
-
-            loss = finish_episode(criticNet, actorNet, optimizer_critic,
-                                  optimizer_actor, gamma)
-
-            history["{}".format(i)]['epoch'].append(episode+1)
-            history["{}".format(i)]['result_efficiency'].append(reward)
-
-            if episode % 1000 == 0:
-                print("episode:{} total reward:{}".format(episode, reward))
-
-        env.close()
-        plot_efficiency_history(history["{}".format(i)], os.path.join(
-            log_dir, 'learning_effi_curve{}.png'.format(i)))
-
-    mean = np.stack([history["{}".format(i)]['result_efficiency']
-                     for i in range(test_num)])
-    std = np.std(mean[:, -1])
-    print('最終結果の標準偏差：', std)
-    mean = np.mean(mean, axis=0)
-
-    meanhistory = {}
-    meanhistory['epoch'] = history['0']['epoch']
-    meanhistory['result_efficiency'] = mean
-
-    # 学習履歴を保存
-    with open(os.path.join(log_dir, 'history.pkl'), 'wb') as f:
-        pickle.dump(history, f)
-
-    plot_efficiency_history(meanhistory, os.path.join(
-        log_dir, 'mean_learning_effi_curve.png'))
-"""
