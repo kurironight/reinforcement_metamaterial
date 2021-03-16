4c2d3a55bc57c302c8a000be183eb454bd84ab2e
kurironight <titechcycsin17@gmail.com>
2021-03-17 07:17:59 +0900

    criticにGCNを用いた

--- confirm/step2/actor_critic.py
+++ confirm/step2/actor_critic.py
@@ -1,19 +1,12 @@
-from itertools import islice
-import random
-from collections import deque, namedtuple
-import copy
-import gym
-import pickle
 import os
 import numpy as np
-from collections import namedtuple
-from policy import model
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 from torch.distributions import Categorical
-import torch.distributions as tdist
+from GCN.layer import CensNet
+from tools.graph import make_T_matrix, make_edge_adj, make_D_matrix
+from collections import namedtuple
 
 Saved_mean_std_Action = namedtuple(
     'SavedAction', ['mean', 'variance'])
@@ -96,11 +89,105 @@ class CriticNetwork(nn.Module):
         return y
 
 
+class CriticNetwork_GCN(torch.nn.Module):
+    def __init__(self, node_in_features, edge_in_features, node_out_features,
+                 edge_out_features):
+        super(CriticNetwork_GCN, self).__init__()
+        self.GCN1 = CensNet(node_in_features, edge_in_features, node_out_features,
+                            edge_out_features)
+        self.GCN2 = CensNet(node_out_features, edge_out_features,
+                            node_out_features, edge_out_features)
+        self.predict_v1 = torch.nn.Linear(node_out_features, node_out_features)
+        self.predict_v2 = torch.nn.Linear(node_out_features, 1)
+
+        self.saved_actions = []
+        self.rewards = []
+
+    def forward(self, node, edge, node_adj, edge_adj, D_v, D_e, T):
+        """
+        forward of both actor and critic
+        """
+        node, edge = self.GCN1(node, edge, node_adj, edge_adj, D_v, D_e, T)
+        # node, edge = adopt_batch_norm(node, edge, self.b1, self.b2)
+        node, edge = self.GCN2(node, edge, node_adj, edge_adj, D_v, D_e, T)
+        # node, edge = adopt_batch_norm(node, edge, self.b3, self.b4)
+        value = F.relu(self.predict_v1(node))  # 1*node_num*node_out_features
+        value = torch.mean(value, dim=1)  # 1*node_out_features
+        value = self.predict_v2(value)  # 1*1
+
+        return value
+
+
+def make_torch_type_for_GCN(nodes_pos, edges_indices, edges_thickness, node_adj):
+    # GCNの為の引数を作成
+    T = make_T_matrix(edges_indices)
+    edge_adj = make_edge_adj(edges_indices, T)
+    D_e = make_D_matrix(edge_adj)
+    D_v = make_D_matrix(node_adj)
+
+    # GCNへの変換
+    node = torch.from_numpy(nodes_pos).clone().double()
+    node = node.unsqueeze(0)
+    edge = torch.from_numpy(edges_thickness).clone().double()
+    edge = edge.unsqueeze(0).unsqueeze(2)
+    node_adj = torch.from_numpy(node_adj).clone().double()
+    node_adj = node_adj.unsqueeze(0)
+    edge_adj = torch.from_numpy(edge_adj).clone().double()
+    edge_adj = edge_adj.unsqueeze(0)
+    D_v = torch.from_numpy(D_v).clone().double()
+    D_v = D_v.unsqueeze(0)
+    D_e = torch.from_numpy(D_e).clone().double()
+    D_e = D_e.unsqueeze(0)
+    T = torch.from_numpy(T).clone().double()
+    T = T.unsqueeze(0)
+
+    return node, edge, node_adj, edge_adj, D_v, D_e, T
+
+
 def select_action(state, node1Net, node2Net, criticNet, device):
     "GCNを用いない手法"
     state_tensor = torch.tensor(
         state, dtype=torch.float, device=device).view(1, 4, 2)
     state_value = criticNet(state_tensor)
+    node1_prob = node1Net(state_tensor)
+    node1_categ = Categorical(node1_prob)
+    node1 = node1_categ.sample()
+    non_node1_state = torch.cat(
+        [state_tensor[:, 0:node1, :], state_tensor[:, node1+1:, :]], 1)
+    node2_prob = node2Net(non_node1_state)
+    node2_categ = Categorical(node2_prob)
+    node2_temp = node2_categ.sample()
+
+    if node2_temp >= node1:
+        node2 = node2_temp+1  # node1分の調整
+    else:
+        node2 = node2_temp
+
+    action = {}
+    action['which_node'] = np.array([node1.item(), node2.item()])
+    action['end'] = 0
+    action['edge_thickness'] = np.array([1])
+    action['new_node'] = np.array([[0, 2]])
+
+    # save to action buffer
+    criticNet.saved_actions.append(Saved_Action(action, state_value))
+    node1Net.saved_actions.append(Saved_prob_Action(
+        node1_categ.log_prob(node1)))
+    node2Net.saved_actions.append(Saved_prob_Action(
+        node2_categ.log_prob(node2_temp)))
+
+    return action
+
+
+def select_action_critic_gcn(env, node1Net, node2Net, criticNet, device):
+    nodes_pos, edges_indices, edges_thickness, node_adj = env.extract_node_edge_info()
+    node, edge, node_adj, edge_adj, D_v, D_e, T = make_torch_type_for_GCN(
+        nodes_pos, edges_indices, edges_thickness, node_adj)
+
+    state_tensor = torch.tensor(
+        nodes_pos, dtype=torch.float, device=device).view(1, 4, 2)
+    state_value = criticNet(node, edge, node_adj,
+                            edge_adj, D_v, D_e, T)
     node1_prob = node1Net(state_tensor)
     node1_categ = Categorical(node1_prob)
     node1 = node1_categ.sample()
@@ -151,7 +238,7 @@ def finish_episode(Critic, node1Net, node2Net, Critic_opt, Node1_opt, Node2_opt,
     for (action, value),  node1_prob, node2_prob,   R in zip(GCN_saved_actions, node1Net_saved_actions, node2Net_saved_actions, returns):
 
         advantage = R - value.item()
-        advantage = advantage.to(torch.float)
+        advantage = advantage.to(torch.float)  # なぜかfloatにしないとエラーを吐いた．
         # print("advantage:", advantage)
 
         # calculate actor (policy) loss
