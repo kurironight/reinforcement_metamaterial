f90bec56be1f575c39fcced74c5cfd3504c88c78
kurironight <titechcycsin17@gmail.com>
2021-05-05 18:02:56 +0900

    epochの調査範囲を指定できるようにした

--- confirm/step3_2/examine_possibility_distribution.py
+++ confirm/step3_2/examine_possibility_distribution.py
@@ -21,32 +21,38 @@ def examine_possibility_distribution(history):
     return A_pos_a_small_to_mean_rate, A_pos_a_big_to_mean_rate, A_neg_a_big_to_mean_rate, A_neg_a_small_to_mean_rate
 
 
-def examine_pattern1_possibility_distribution(history):
+def examine_pattern1_possibility_distribution(history, epoch_start_end=None):
     """パターン１発生時のa_mean<aの時などの各事象の発生率を見る
+
+    Args:
+        history ([dictionary]): 学習の経緯を保存している辞書
+        epoch_start_end ([tuple], optional): (start_epoch,end_epoch)という形式で，historyの内の調査範囲を指定することができる. Defaults to None.
 
     Returns:
         A>0&a_mean>a，A>0&a_mean<aが出現する確率（母数は全体のepoch数-1）を出力
     """
-    data_num = len(history['epoch'])  # 本来なら-1すべきかもしれないが，母数を合わせる為にそのままにする
-    future_history_a_mean = np.array(history['a_mean'][1:])
-    current_history_a_mean = np.array(history['a_mean'][:-1])
-    current_history_advantage = np.array(history['advantage'][:-1])
-    current_history_a = np.array(history['a'][:-1])
+    if epoch_start_end is not None:
+        epoch_range = list(range(epoch_start_end[0] - 1, epoch_start_end[1]))
+    else:
+        epoch_range = list(range(len(history['epoch'])))
 
-    pattern1_occur = current_history_advantage >= 0
-    pattern1_data_num = np.count_nonzero(pattern1_occur)
+    data_num = len(np.array(history['epoch'])[epoch_range])  # 本来なら-1すべきかもしれないが，母数を合わせる為にそのままにする
+    future_history_a_mean = np.array(np.array(history['a_mean'])[epoch_range][1:])
+    current_history_a_mean = np.array(np.array(history['a_mean'])[epoch_range][:-1])
+    current_history_advantage = np.array(np.array(history['advantage'])[epoch_range][:-1])
+    current_history_a = np.array(np.array(history['a'])[epoch_range][:-1])
 
-    A_pos_a_small_to_mean = (pattern1_occur & (current_history_a < current_history_a_mean)) & (future_history_a_mean < current_history_a_mean)
-    A_pos_a_big_to_mean = (pattern1_occur & (current_history_a >= current_history_a_mean)) & (future_history_a_mean >= current_history_a_mean)
+    A_pos_a_small_to_mean = ((current_history_advantage >= 0) & (current_history_a < current_history_a_mean)) & (future_history_a_mean < current_history_a_mean)
+    A_pos_a_big_to_mean = ((current_history_advantage >= 0) & (current_history_a >= current_history_a_mean)) & (future_history_a_mean >= current_history_a_mean)
     A_pos_a_small_to_mean_rate = np.count_nonzero(A_pos_a_small_to_mean) / data_num
     A_pos_a_big_to_mean_rate = np.count_nonzero(A_pos_a_big_to_mean) / data_num
 
-    pattern1_occur_rate = pattern1_data_num / data_num
+    pattern1_occur_rate = A_pos_a_small_to_mean_rate + A_pos_a_big_to_mean_rate
 
     return pattern1_occur_rate, A_pos_a_small_to_mean_rate, A_pos_a_big_to_mean_rate
 
 
-def examine_pattern2_possibility_distribution(history):
+def examine_pattern2_possibility_distribution(history, epoch_start_end=None):
     """パターン２発生時のa_mean<aの時などの各事象の発生率を見る
 
     Returns:
@@ -59,17 +65,19 @@ def examine_pattern2_possibility_distribution(history):
         rewards = pickle.load(web)
         rewards = np.array(rewards).flatten()
 
-    data_num = len(history['epoch'])  # 本来なら-1すべきかもしれないが，母数を合わせる為にそのままにする
-    Vp_a = np.array([convert_Vp_to_edgethick(edge_thicknesses, rewards, Vp) for Vp in np.array(history['critic_value'])])
-    future_Vp_a = Vp_a[1:]
+    if epoch_start_end is not None:
+        epoch_range = list(range(epoch_start_end[0] - 1, epoch_start_end[1]))
+    else:
+        epoch_range = list(range(len(history['epoch'])))
 
-    future_a_mean = np.array(history['a_mean'][1:])
-    future_a = np.array(history['a'][1:])
-    future_Vp = np.array(history['critic_value'][1:])
-    future_Vp_a = np.array([convert_Vp_to_edgethick(edge_thicknesses, rewards, Vp) for Vp in future_Vp])
-    current_a_mean = np.array(history['a_mean'][:-1])
-    current_Vp = np.array(history['critic_value'][:-1])
-    current_Vp_a = np.array([convert_Vp_to_edgethick(edge_thicknesses, rewards, Vp) for Vp in current_Vp])
+    data_num = len(np.array(history['epoch'])[epoch_range])  # 本来なら-1すべきかもしれないが，母数を合わせる為にそのままにする
+    Vp_a = np.array([convert_Vp_to_edgethick(edge_thicknesses, rewards, Vp) for Vp in np.array(np.array(history['critic_value'])[epoch_range])])
+    future_Vp_a = Vp_a[1:]
+    current_Vp_a = Vp_a[:-1]
+
+    future_a_mean = np.array(np.array(history['a_mean'])[epoch_range][1:])
+    future_a = np.array(np.array(history['a'])[epoch_range][1:])
+    current_a_mean = np.array(np.array(history['a_mean'])[epoch_range][:-1])
 
     pattern2_occur = ((current_Vp_a - current_a_mean) * (current_a_mean - future_Vp_a)) > 0
     pattern2_data_num = np.count_nonzero(pattern2_occur)
@@ -107,13 +115,19 @@ def convert_Vp_to_edgethick(edge_thicknesses, rewards, Vp):
     return edge_thick
 
 
-def examine_pattern3_possibility_distribution(history, a_sigma_thresh=6.046791718933308 * 10**(-9)):
-    data_num = len(history['epoch'])  # 本来なら-1すべきかもしれないが，母数を合わせる為にそのままにする
-    current_history_a_sigma = np.array(history['a_sigma'][:-1])
+def examine_pattern3_possibility_distribution(history, a_sigma_thresh=6.046791718933308 * 10**(-9), epoch_start_end=None):
 
-    current_history_a_mean = np.array(history['a_mean'][:-1])
-    current_history_advantage = np.array(history['advantage'][:-1])
-    current_history_a = np.array(history['a'][:-1])
+    if epoch_start_end is not None:
+        epoch_range = list(range(epoch_start_end[0] - 1, epoch_start_end[1]))
+    else:
+        epoch_range = list(range(len(history['epoch'])))
+
+    data_num = len(np.array(history['epoch'])[epoch_range])  # 本来なら-1すべきかもしれないが，母数を合わせる為にそのままにする
+    current_history_a_sigma = np.array(np.array(history['a_sigma'])[epoch_range][:-1])
+
+    current_history_a_mean = np.array(np.array(history['a_mean'])[epoch_range][:-1])
+    current_history_advantage = np.array(np.array(history['advantage'])[epoch_range][:-1])
+    current_history_a = np.array(np.array(history['a'])[epoch_range][:-1])
 
     pattern3_occur = current_history_a_sigma < a_sigma_thresh
     pattern3_data_num = np.count_nonzero(pattern3_occur)
@@ -136,4 +150,6 @@ def examine_pattern3_possibility_distribution(history, a_sigma_thresh=6.04679171
 with open("confirm/step3_2/a_gcn_c_gcn_results/50000確認/history.pkl", 'rb') as web:
     history = pickle.load(web)
 
-print(examine_pattern1_possibility_distribution(history["2"]))
+print(examine_pattern1_possibility_distribution(history["2"], epoch_start_end=None))
+print(examine_pattern2_possibility_distribution(history["2"], (35000, 50000)))
+print(examine_pattern3_possibility_distribution(history["2"], epoch_start_end=None))
