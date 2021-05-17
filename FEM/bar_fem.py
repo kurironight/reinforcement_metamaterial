fa6cdac2dcfb3906edc8be462d62e60fdfb69a29
kmht1919 <keigokeigo1205@yahoo.co.jp>
2021-05-17 16:55:56 +0900

    APDLの精度を確認する関数を実装した

--- FEM/bar_fem.py
+++ FEM/bar_fem.py
@@ -85,6 +85,63 @@ def barfem(nodes_pos, edges_indices, edges_thickness, input_nodes, input_vectors
     return displacement
 
 
+def confirm_apdl_accuracy(nodes_pos, edges_indices, edges_thickness, input_nodes, input_vectors, frozen_nodes, displacement):
+    """APDLのソルバーの精度を測る.
+    displacementには，apdlで出力された変位を利用する．
+    """
+    node_num = nodes_pos.shape[0]
+    edge_num = edges_indices.shape[0]
+    input_node_num = len(input_nodes)
+    frozen_node_num = len(frozen_nodes)
+    nodes_pos = nodes_pos.astype(
+        np.float64)
+    input_vectors = input_vectors.astype(
+        np.float64)
+    edges_indices = edges_indices.astype(
+        np.int32)  # ここをint32型にしないとコードが正しく作動しない
+    edges_thickness = edges_thickness.astype(
+        np.float64)  # ここをfloat64型にしないとコードが正しく作動しない
+
+    assert input_vectors.shape[1] == 2, 'input_vectorsの形は[[[0, -1]],[0,3]]のような形である'
+    assert edges_indices.shape[1] == 2, 'edges_indicesの形は[[0,1],[2,3]]のような形である'
+
+    assert np.all(np.isin(np.arange(node_num), edges_indices)
+                  ), 'edge_indicesでは触れられていないノードが存在する'
+
+    # doubleのポインタのポインタ型を用意
+    _DOUBLE_PP = ndpointer(dtype=np.uintp, ndim=1, flags='C')
+    # 関数の引数の型を指定(ctypes)　
+    libc.confirm_apdl_accuracy.argtypes = [_DOUBLE_PP, _DOUBLE_PP, _DOUBLE_PP, ct.c_int32, ct.c_int32, ct.c_int32,
+                                           ctypes.POINTER(ctypes.c_int), _DOUBLE_PP, ct.c_int32, ctypes.POINTER(ctypes.c_int), _DOUBLE_PP]
+    # 関数が返す値の型を指定(今回は返り値なし)
+    libc.confirm_apdl_accuracy.restype = None
+
+    # 各配列のアドレスを求めている
+    tp = np.uintp
+
+    mpp = (nodes_pos.__array_interface__[
+           'data'][0] + np.arange(nodes_pos.shape[0]) * nodes_pos.strides[0]).astype(tp)
+    eipp = (edges_indices .__array_interface__[
+        'data'][0] + np.arange(edges_indices .shape[0]) * edges_indices.strides[0]).astype(tp)
+    etpp = (edges_thickness.__array_interface__[
+        'data'][0] + np.arange(edges_thickness.shape[0]) * edges_thickness.strides[0]).astype(tp)
+    dspp = (displacement.__array_interface__[
+        'data'][0] + np.arange(displacement.shape[0]) * displacement.strides[0]).astype(tp)
+    ivpp = (input_vectors.__array_interface__[
+        'data'][0] + np.arange(input_vectors.shape[0]) * input_vectors.strides[0]).astype(tp)
+
+    # int型もctypeのc_int型へ変換して渡す
+    cnode_num = ctypes.c_int(node_num)
+    cedge_num = ctypes.c_int(edge_num)
+    cinput_node_num = ctypes.c_int(input_node_num)
+    cfrozen_node_num = ctypes.c_int(frozen_node_num)
+
+    inp = (ctypes.c_int * len(input_nodes))(*input_nodes)
+    frz = (ctypes.c_int * len(frozen_nodes))(*frozen_nodes)
+    libc.confirm_apdl_accuracy(mpp, eipp, etpp, cnode_num, cedge_num, cinput_node_num, inp, ivpp, cfrozen_node_num,
+                               frz, dspp)
+
+
 """
 使用例
 
