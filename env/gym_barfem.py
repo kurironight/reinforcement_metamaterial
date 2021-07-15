75faeb8b2e8e383e80ff8066aceccd80b52ac532
kurironight <titechcycsin17@gmail.com>
2021-07-15 15:31:01 +0900

    表示部分，最大幅修正

--- env/gym_barfem.py
+++ env/gym_barfem.py
@@ -62,7 +62,7 @@ class BarFemGym(MetamechGym):
             display_number (bool, optional): ノードに番号をつけるか付けないか. Defaults to False.
         """
 
-        edge_size = 15  # 図示する時のエッジの太さ
+        edge_size = 30  # 図示する時のエッジの太さ
         marker_size = 40  # 図示するときのノードのサイズ
         character_size = 20  # ノードの文字のサイズ
 
