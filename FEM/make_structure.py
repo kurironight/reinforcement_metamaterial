7f91d2a382d58fd0d76a0c29851f58969c68d393
kurironight <titechcycsin17@gmail.com>
2021-09-22 22:32:33 +0900

    ピクセルでの表現関数に対してdocを修正した

--- FEM/make_structure.py
+++ FEM/make_structure.py
@@ -5,9 +5,9 @@ def make_bar_structure(x_size, y_size, edges):
     """edgesを基に，バーを用いた構造を作成する
 
     Args:
-        x_size (float): 目標とする1/4スケールの構造物のx方向のブロック数
-        y_size (float): 目標とする1/4スケールの構造物のy方向のブロック数
-        edges (list): [エッジの始点，終点，太さ]のリスト
+        x_size (float): x方向のブロック数
+        y_size (float): y方向のブロック数
+        edges (list): [エッジの始点，終点，太さ]のリスト．この値の単位は1ブロック．
 
     Returns:
         np.array (y_size * x_size): バー構造
@@ -33,40 +33,40 @@ def put_bar(rho, start_point, end_point, width):
     if np.all(start_point == end_point):
         return rho
     else:
-        edge_point1 = end_point+(end_point-start_point) / \
-            np.linalg.norm(end_point - start_point, ord=2)*0.5
+        edge_point1 = end_point + (end_point - start_point) / \
+            np.linalg.norm(end_point - start_point, ord=2) * 0.5
         edge_point2 = start_point + \
-            (start_point-end_point) / \
-            np.linalg.norm(end_point - start_point, ord=2)*0.5
+            (start_point - end_point) / \
+            np.linalg.norm(end_point - start_point, ord=2) * 0.5
 
     # xの方でメッシュの座標は[0,.....,rho.shape[1]]．なお，数はrho.shape[1]＋１
     x_index = np.linspace(0, rho.shape[1], num=rho.shape[1], dtype=np.float64)
     y_index = np.linspace(0, rho.shape[0], num=rho.shape[0], dtype=np.float64)
     xx, yy = np.meshgrid(x_index, y_index)
-    if (end_point[0]-start_point[0]) != 0:  # x=8等のような直線の式にならない場合
-        m = (end_point[1]-start_point[1])/(end_point[0]-start_point[0])
-        n = end_point[1]-m*end_point[0]
-        d = np.abs(yy-m*xx-n)/np.sqrt(1+np.power(m, 2))
+    if (end_point[0] - start_point[0]) != 0:  # x=8等のような直線の式にならない場合
+        m = (end_point[1] - start_point[1]) / (end_point[0] - start_point[0])
+        n = end_point[1] - m * end_point[0]
+        d = np.abs(yy - m * xx - n) / np.sqrt(1 + np.power(m, 2))
         # 垂線の足を求める
-        X = (m*(yy-n)+xx)/(np.power(m, 2)+1)
-        Y = m*X+n
+        X = (m * (yy - n) + xx) / (np.power(m, 2) + 1)
+        Y = m * X + n
         # バーを配置できる条件を満たすインデックスを求める
         X_on_segment = np.logical_and(min(
             edge_point1[0], edge_point2[0]) <= X, X <= max(edge_point1[0], edge_point2[0]))
         Y_on_segment = np.logical_and(min(
             edge_point1[1], edge_point2[1]) <= Y, Y <= max(edge_point1[1], edge_point2[1]))
         on_segment = np.logical_and(X_on_segment, Y_on_segment)
-        in_distance = d <= width/2
+        in_distance = d <= width / 2
         meet_index = np.logical_and(on_segment, in_distance)
     else:  # x=8等の場合
-        d = np.abs(end_point[0]-xx)
+        d = np.abs(end_point[0] - xx)
         # 垂線の足を求める
         X = end_point[0]
         Y = yy
         # バーを配置できる条件を満たすインデックスを求める
         Y_on_segment = np.logical_and(min(
             edge_point1[1], edge_point2[1]) <= Y, Y <= max(edge_point1[1], edge_point2[1]))
-        in_distance = d <= width/2
+        in_distance = d <= width / 2
         meet_index = np.logical_and(Y_on_segment, in_distance)
     rho[meet_index] = 1
     return rho
