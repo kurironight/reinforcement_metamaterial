d61baf04e0f44f8ca07bfa5462b16016f72c4aa9
kurironight <titechcycsin17@gmail.com>
2021-09-22 21:43:49 +0900

    リファクタリング2

--- tools/save.py
+++ tools/save.py
@@ -55,3 +55,29 @@ def load_graph_info(log_dir):
         frozen_nodes = pickle.load(f)
     return nodes_positions, input_nodes, input_vectors, output_nodes,\
         output_vectors, frozen_nodes, edges_indices, edges_thickness
+
+
+def save_graph_info_npy(log_dir, nodes_positions, input_nodes, input_vectors,
+                        output_nodes, output_vectors, frozen_nodes,
+                        edges_indices, edges_thickness):
+    """グラフの情報をlog_dir内部にnpy形式で保存する保存する．
+
+    Args:
+        log_dir (str)
+        nodes_positions (np.array)
+        input_nodes (list)
+        input_vectors (np.array)
+        output_nodes (list)
+        output_vectors (np.array)
+        frozen_nodes (list)
+        edges_indices (np.array)
+        edges_thickness (np.array)
+    """
+    np.save(os.path.join(log_dir, "nodes_pos.npy"), nodes_positions)
+    np.save(os.path.join(log_dir, "edges_indices.npy"), edges_indices)
+    np.save(os.path.join(log_dir, "edges_thickness.npy"), edges_thickness)
+    np.save(os.path.join(log_dir, "input_nodes.npy"), input_nodes)
+    np.save(os.path.join(log_dir, "input_vectors.npy"), input_vectors)
+    np.save(os.path.join(log_dir, "frozen_nodes.npy"), frozen_nodes)
+    np.save(os.path.join(log_dir, "output_nodes.npy"), output_nodes)
+    np.save(os.path.join(log_dir, "output_vectors.npy"), output_vectors)
