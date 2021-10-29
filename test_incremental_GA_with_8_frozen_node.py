600bcc057cb138ee0ed1a3844f69d269ab4f23b4
yuki_uchida <uchida.y.am@m.titech.ac.jp>
2021-10-29 15:30:15 +0900

    実行用関数の修正

--- test_incremental_GA_with_8_frozen_node.py
+++ test_incremental_GA_with_8_frozen_node.py
@@ -1,15 +1,17 @@
+import pickle
+from tqdm import tqdm
+import os
+import matplotlib.pyplot as plt
 from numpy.core.numeric import _cross_dispatcher
 from platypus import NSGAII, DTLZ2, ProcessPoolEvaluator
 from platypus import NSGAII, Problem, nondominated, Integer, Real, \
     CompoundOperator, SBX, HUX, UM, BitFlip, GeneticAlgorithm, PM
-from GA.GA_class import StressConstraint_GA
-from GA.algorithm import FixNode_NSGAII
+from GA.GA_class import *
+from GA.algorithm import FixNode_NSGAII, FixNode_add_middle_point_NSGAII
 import time
 import numpy as np
-import matplotlib.pyplot as plt
-import os
-from tqdm import tqdm
-import pickle
+import matplotlib
+matplotlib.use('Agg')
 
 max_free_node_num = 18
 free_nodes_num = np.arange(1, max_free_node_num + 1)
@@ -17,15 +19,17 @@ fix_nodes_num = np.ones(free_nodes_num.shape, dtype=np.int) * 8
 
 """
 # 単体実行用
-max_free_node_num = 18
+max_free_node_num = 4
 free_nodes_num = np.arange(max_free_node_num, max_free_node_num + 1)
 fix_nodes_num = np.ones(free_nodes_num.shape, dtype=np.int) * 8
 """
 experient_num = 3
+GA_type = FixnodeForceDisp_GA
+algorithm_type = FixNode_add_middle_point_NSGAII
 
 if __name__ == "__main__":
     # define the problem definition
-    save_dir = "GA/result/18段階的GA"
+    save_dir = "GA/result/エッジ中間追加_ノード固定有η3"
     generation = 30
     save_interval = 5
 
@@ -40,21 +44,21 @@ if __name__ == "__main__":
         start = time.time()
         # instantiate the optimization algorithm to run in parallel
         for index, (free_node_num, fix_node_num) in enumerate(zip(free_nodes_num, fix_nodes_num)):
-            problem = StressConstraint_GA(free_node_num, fix_node_num)
+            problem = GA_type(free_node_num, fix_node_num)
             parent = problem.nvars * parent_mult_value
             history = []
             GA_result_dir = os.path.join(PATH, "free_{}_fix_{}".format(free_node_num, fix_node_num))
             os.makedirs(GA_result_dir, exist_ok=True)
             with ProcessPoolEvaluator(8) as evaluator:
                 if index == 0:  # 一つ目のGAでは遺伝子を引き継がない
-                    problem = StressConstraint_GA(free_node_num, fix_node_num)
+                    problem = GA_type(free_node_num, fix_node_num)
                     algorithm = NSGAII(problem, population_size=parent, variator=CompoundOperator(SBX(), HUX(), PM(), BitFlip()), evaluator=evaluator)
                 else:  # 二回目以降のGAでは遺伝子を引き継ぐ
                     load_GA_result_dir = os.path.join(PATH, "free_{}_fix_{}".format(free_nodes_num[index - 1], fix_nodes_num[index - 1]))
                     load_gene_path = os.path.join(load_GA_result_dir, "parents.pk")
-                    problem = StressConstraint_GA(free_node_num, fix_node_num)
-                    prior_problem = StressConstraint_GA(free_nodes_num[index - 1], fix_nodes_num[index - 1])
-                    algorithm = FixNode_NSGAII(problem, prior_problem, gene_path=load_gene_path, population_size=parent, variator=CompoundOperator(SBX(), HUX(), PM(), BitFlip()), evaluator=evaluator)
+                    problem = GA_type(free_node_num, fix_node_num)
+                    prior_problem = GA_type(free_nodes_num[index - 1], fix_nodes_num[index - 1])
+                    algorithm = algorithm_type(problem, prior_problem, gene_path=load_gene_path, population_size=parent, variator=CompoundOperator(SBX(), HUX(), PM(), BitFlip()), evaluator=evaluator)
                 for i in tqdm(range(generation)):
                     algorithm.step()
                     efficiency_results = [s.objectives[0] for s in algorithm.result if s.feasible]  # 実行可能解しか抽出しない
@@ -75,7 +79,7 @@ if __name__ == "__main__":
                     plt.savefig(os.path.join(GA_result_dir, "history.png"))
                     plt.close()
 
-                    if algorithm.nfe / parent % save_interval == 0:
+                    if (i + 1) % save_interval == 0:
                         save_log_dir = os.path.join(GA_result_dir, str(i + 1))
                         if not os.path.exists(save_log_dir):
                             os.makedirs(save_log_dir)
