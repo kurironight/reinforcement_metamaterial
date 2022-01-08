824a3f3daf8abfb08c2703500d2ea843dc2d5476
kurironight <titechcycsin17@gmail.com>
2022-01-09 01:04:40 +0900

    step3の修正

--- confirm/step3/actor_critic.py
+++ confirm/step3/actor_critic.py
@@ -8,6 +8,8 @@ from GCN.layer import CensNet
 from tools.graph import make_T_matrix, make_edge_adj, make_D_matrix
 from collections import namedtuple
 import torch.distributions as tdist
+from .condition import easy_dev
+from env.gym_barfem import BarFemGym
 
 Saved_mean_std_Action = namedtuple(
     'SavedAction', ['mean', 'variance'])
@@ -21,10 +23,10 @@ def init_weight(size):
     return torch.tensor(np.random.uniform(low=-v, high=v, size=size), dtype=torch.float)
 
 
-class ActorNetwork_GCN(nn.Module):
+class Select_node1_model(nn.Module):
     def __init__(self, node_in_features, edge_in_features, node_out_features,
                  edge_out_features):
-        super(ActorNetwork_GCN, self).__init__()
+        super(Select_node1_model, self).__init__()
         self.GCN1 = CensNet(node_in_features, edge_in_features, node_out_features,
                             edge_out_features)
         self.GCN2 = CensNet(node_out_features, edge_out_features,
@@ -40,18 +42,40 @@ class ActorNetwork_GCN(nn.Module):
         forward of both actor and critic
         """
         node, edge = self.GCN1(node, edge, node_adj, edge_adj, D_v, D_e, T)
-        # node, edge = adopt_batch_norm(node, edge, self.b1, self.b2)
         node, edge = self.GCN2(node, edge, node_adj, edge_adj, D_v, D_e, T)
-        # node, edge = adopt_batch_norm(node, edge, self.b3, self.b4)
+        emb_node = node.clone()
         node = F.relu(self.predict_v1(node))  # 1*node_num*node_out_features
         node = self.predict_v2(node)  # 1*node_num
         node = node.reshape((-1, node.size(1)))
         if remove_index is not False:
             node = torch.cat(
-                [node[:, 0:remove_index], node[:, remove_index+1:]], 1)
+                [node[:, 0:remove_index], node[:, remove_index + 1:]], 1)
         node = F.softmax(node, dim=-1)
 
-        return node
+        return emb_node, node
+
+
+class Select_node2_model(torch.nn.Module):
+    def __init__(self, node_in_features, emb_size):
+        super(Select_node2_model, self).__init__()
+        self.layer1 = torch.nn.Linear(node_in_features, emb_size)
+        self.layer2 = torch.nn.Linear(emb_size, 1)
+        self.layer3 = torch.nn.Linear(emb_size, emb_size)
+
+        # action & reward buffer
+        self.saved_actions = []
+
+    def forward(self, emb_node):
+        x = F.relu(self.layer1(emb_node))  # 1*node_num*emb_size
+        emb_node = x.clone()
+        emb_node = self.layer3(emb_node)  # 1*node_num*emb_size
+        emb_node = torch.mean(emb_node, dim=1)  # 1*emb_size
+
+        x = self.layer2(x)  # 1*node_num*1
+        x = x.reshape((1, -1))
+        x = F.softmax(x, dim=-1)  # 1*node_num
+
+        return emb_node, x
 
 
 class CriticNetwork_GCN(torch.nn.Module):
@@ -84,37 +108,22 @@ class CriticNetwork_GCN(torch.nn.Module):
 
 
 class Edgethick_Actor(nn.Module):
-    """入力がstate.
-    """
 
-    def __init__(self, node_in_features, edge_in_features, node_out_features,
-                 edge_out_features):
-        super(Edgethick_Actor, self).__init__()
-        self.GCN1 = CensNet(node_in_features, edge_in_features, node_out_features,
-                            edge_out_features)
-        self.GCN2 = CensNet(node_out_features, edge_out_features,
-                            node_out_features, edge_out_features)
-        self.predict_v1 = torch.nn.Linear(node_out_features, node_out_features)
-        self.mean = torch.nn.Linear(4*node_out_features, 1)
-        self.std = torch.nn.Linear(4*node_out_features, 1)
-
+    def __init__(self, in_features, hidden1_size=400, hidden2_size=300):
+        self.fc1 = nn.Linear(in_features, hidden1_size)
+        self.fc2 = nn.Linear(hidden1_size, hidden2_size)
+        self.mean = nn.Linear(hidden2_size, 1)
+        self.std = nn.Linear(hidden2_size, 1)
         self.saved_actions = []
 
-    def forward(self, node, edge, node_adj, edge_adj, D_v, D_e, T, node1, node2):
-        """
-        forward of both actor and critic
-        """
-        node, edge = self.GCN1(node, edge, node_adj, edge_adj, D_v, D_e, T)
-        node, edge = self.GCN2(node, edge, node_adj, edge_adj, D_v, D_e, T)
-        node = F.relu(self.predict_v1(node))  # 1*node_num*node_out_features
-        node1_2 = node[:, [node1, node2], :]  # 1*2*node_out_features
-        node1_2 = node.reshape(1, -1)  # 1*(2*node_out_features)
-
-        # var to mean todekaeru
-        mean = 0.1+0.9*torch.sigmoid(self.mean(node1_2))
-        std = 0.1*torch.sigmoid(self.std(node1_2))
+    def forward(self, x):
+        h = F.relu(self.fc1(x))
+        h = F.relu(self.fc2(h))
+        mean = self.mean(h)
+        std = self.std(h)
+        mean = torch.abs(mean)
+        std = torch.abs(std)
         y = torch.cat([mean, std], dim=1)
-
         return y
 
 
@@ -144,31 +153,40 @@ def make_torch_type_for_GCN(nodes_pos, edges_indices, edges_thickness, node_adj)
     return node, edge, node_adj, edge_adj, D_v, D_e, T
 
 
-def select_action_gcn_critic_gcn(env, node1Net, node2Net, criticNet, edgethickNet, device, log_dir=None):
+def select_action_gcn_critic_gcn(env, node1Net, node2Net, criticNet, edgethickNet, device, log_dir=None, history=None):
     nodes_pos, edges_indices, edges_thickness, node_adj = env.extract_node_edge_info()
+    node_num = nodes_pos.shape[0]
     node, edge, node_adj, edge_adj, D_v, D_e, T = make_torch_type_for_GCN(
         nodes_pos, edges_indices, edges_thickness, node_adj)
     state_value = criticNet(node, edge, node_adj,
                             edge_adj, D_v, D_e, T)
-    node1_prob = node1Net(node, edge, node_adj,
-                          edge_adj, D_v, D_e, T)
+    # ノード1を求める
+    emb_node, node1_prob = node1Net(node, edge, node_adj,
+                                    edge_adj, D_v, D_e, T)
     node1_categ = Categorical(node1_prob)
     node1 = node1_categ.sample()
-    label = torch.zeros(1, 4, 1).double()
-    label[:, node1, :] = 1
 
-    node_labeled = torch.cat([node, label], 2)
-    node2_prob = node2Net(node_labeled, edge, node_adj,
-                          edge_adj, D_v, D_e, T, remove_index=node1)
+    # ノード1を除いたnodeの作成
+    non_node1_node = torch.cat(
+        [node[:, 0:node1, :], node[:, node1 + 1:, :]], 1)
+
+    # ノード1の情報抽出
+    H1 = emb_node[0][node1]
+    H1_cat = H1.repeat(node_num, 1)
+    H1_cat = H1_cat.unsqueeze(0)
+    # HとH1のノード情報をconcat
+    emb_graph_cat = torch.cat([non_node1_node, H1_cat], 2)
+
+    # ノード2を求める
+    emb_edge, node2_prob = node2Net(emb_graph_cat)
     node2_categ = Categorical(node2_prob)
     node2_temp = node2_categ.sample()
-
     if node2_temp >= node1:
-        node2 = node2_temp+1  # node1分の調整
+        node2 = node2_temp + 1  # node1分の調整
     else:
         node2 = node2_temp
-    edge_thickness = edgethickNet(node, edge, node_adj,
-                                  edge_adj, D_v, D_e, T, node1, node2)
+
+    edge_thickness = edgethickNet(emb_edge)
     edge_thickness_tdist = tdist.Normal(
         edge_thickness[0][0].item(), edge_thickness[0][1].item())
     edge_thickness_action = edge_thickness_tdist.sample()
@@ -189,7 +207,7 @@ def select_action_gcn_critic_gcn(env, node1Net, node2Net, criticNet, edgethickNe
     edgethickNet.saved_actions.append(Saved_mean_std_Action(
         edge_thickness[0][0], edge_thickness[0][1]))
 
-    if log_dir != None:
+    if log_dir is not None:
         # lossの確認事項
         with open(os.path.join(log_dir, "progress.txt"), mode='a') as f:
             print('node1_prob:', node1_prob, file=f)
@@ -199,11 +217,33 @@ def select_action_gcn_critic_gcn(env, node1Net, node2Net, criticNet, edgethickNe
             print('edge_thick_mean:', edge_thickness[0][0].item(), file=f)
             print('edge_thick_std:', edge_thickness[0][1].item(), file=f)
             print('edge_thickness:', edge_thickness_action.item(), file=f)
+    if history is not None:
+        node_pos, input_nodes, input_vectors,\
+            output_nodes, output_vectors, frozen_nodes,\
+            edges_indices, edges_thickness, frozen_nodes = easy_dev()
+        calc_effi_env = BarFemGym(node_pos, input_nodes, input_vectors,
+                                  output_nodes, output_vectors, frozen_nodes,
+                                  edges_indices, edges_thickness, frozen_nodes)
+        calc_effi_env.reset()
+        mean_action = {}
+        mean_action['which_node'] = np.array([node1, node2])
+        mean_action['end'] = 0
+        mean_action['edge_thickness'] = np.array([edge_thickness[0][0].item()])
+        mean_action['new_node'] = np.array([[0, 2]])
+        next_nodes_pos, _, done, _ = calc_effi_env.step(mean_action)
+        mean_efficiency = calc_effi_env.calculate_simulation(mode='force')
+
+        # historyにログを残す
+        history['mean_efficiency'].append(mean_efficiency)
+        history['a'].append(edge_thickness_action.item())
+        history['a_mean'].append(edge_thickness[0][0].item())
+        history['a_sigma'].append(edge_thickness[0][1].item())
+        history['critic_value'].append(state_value.item())
 
     return action
 
 
-def finish_episode(Critic, node1Net, node2Net, edgethickNet, Critic_opt, Node1_opt, Node2_opt, Edge_thick_opt, gamma, log_dir=None):
+def finish_episode(Critic, node1Net, node2Net, edgethickNet, Critic_opt, Node1_opt, Node2_opt, Edge_thick_opt, gamma, log_dir=None, history=None):
     R = 0
     GCN_saved_actions = Critic.saved_actions
     node1Net_saved_actions = node1Net.saved_actions
@@ -221,7 +261,8 @@ def finish_episode(Critic, node1Net, node2Net, edgethickNet, Critic_opt, Node1_o
         returns.insert(0, R)
     returns = torch.tensor(returns)
 
-    for (action, value), (edge_thick_mean, edge_thick_std), node1_prob, node2_prob,   R in zip(GCN_saved_actions, Edge_thickness_saved_actions, node1Net_saved_actions, node2Net_saved_actions, returns):
+    edge_thick_opt_trigger = False  # advantage>0の場合したときにEdge_thick_optを作動出来るようにする為のトリガー
+    for (action, value), (edge_thick_mean, edge_thick_std), node1_prob, node2_prob, R in zip(GCN_saved_actions, Edge_thickness_saved_actions, node1Net_saved_actions, node2Net_saved_actions, returns):
 
         advantage = R - value.item()
 
@@ -238,9 +279,11 @@ def finish_episode(Critic, node1Net, node2Net, edgethickNet, Critic_opt, Node1_o
                 edge_thick_mean_loss = F.l1_loss(torch.from_numpy(
                     action["edge_thickness"]).double(), edge_thick_mean.reshape((1)).double())
                 edge_thick_var_loss = F.l1_loss(torch.from_numpy(
-                    np.abs(action["edge_thickness"]-edge_thick_mean.item())).double(), edge_thick_std.reshape((1)).double())
+                    np.abs(action["edge_thickness"] - edge_thick_mean.item())).double(), edge_thick_std.reshape((1)).double())
                 policy_losses.append(
-                    (edge_thick_mean_loss+edge_thick_var_loss) * advantage)
+                    (edge_thick_mean_loss + edge_thick_var_loss) * advantage)
+
+                edge_thick_opt_trigger = True  # Edge_thick_optのトリガーを起動
             else:
                 edge_thick_mean_loss = torch.zeros(1)
                 edge_thick_var_loss = torch.zeros(1)
@@ -249,14 +292,12 @@ def finish_episode(Critic, node1Net, node2Net, edgethickNet, Critic_opt, Node1_o
         value_losses.append(
             F.l1_loss(value.double(), torch.tensor([[R]]).double()))
 
-    # print("policy_losses:", policy_losses)
-    # print("value_losses:", value_losses)
-
     # reset gradients
     Critic_opt.zero_grad()
     Node1_opt.zero_grad()
     Node2_opt.zero_grad()
-    Edge_thick_opt.zero_grad()
+    if edge_thick_opt_trigger:
+        Edge_thick_opt.zero_grad()
 
     # sum up all the values of policy_losses and value_losses
     if len(policy_losses) == 0:
@@ -270,7 +311,8 @@ def finish_episode(Critic, node1Net, node2Net, edgethickNet, Critic_opt, Node1_o
     Critic_opt.step()
     Node1_opt.step()
     Node2_opt.step()
-    Edge_thick_opt.step()
+    if edge_thick_opt_trigger:
+        Edge_thick_opt.step()
 
     # reset rewards and action buffer
     del Critic.rewards[:]
@@ -279,13 +321,15 @@ def finish_episode(Critic, node1Net, node2Net, edgethickNet, Critic_opt, Node1_o
     del node2Net.saved_actions[:]
     del edgethickNet.saved_actions[:]
 
-    if log_dir != None:
+    if log_dir is not None:
         # lossの確認事項
         with open(os.path.join(log_dir, "progress.txt"), mode='a') as f:
             f.writelines('reward: %.4f\n value_loss: %.4f policy_loss: %.4f \n' %
-                         (R, value_losses[0].item(), loss.item()-value_losses[0].item()))
+                         (R, value_losses[0].item(), loss.item() - value_losses[0].item()))
         with open(os.path.join(log_dir, "progress.txt"), mode='a') as f:
             f.writelines('advantage: %.4f log_probs: %.4f edge_thick_mean_loss: %.4f edge_thick_var_loss: %.4f \n' %
                          (advantage, -torch.mean(log_probs).item(), edge_thick_mean_loss.item(), edge_thick_var_loss.item()))
 
+    if history is not None:
+        history['advantage'].append(advantage.item())
     return loss.item()
--- confirm/step3/run.py
+++ confirm/step3/run.py
@@ -7,6 +7,7 @@ from .actor_critic import *
 from env.gym_barfem import BarFemGym
 import numpy as np
 import pickle
+from tqdm import tqdm
 
 
 def confirm_max_status():
@@ -29,29 +30,35 @@ def confirm_max_status():
         action['new_node'] = np.array([[0, 2]])
 
         next_nodes_pos, _, done, _ = env.step(action)
-        reward = env.calculate_simulation(mode='force')
+        reward = env.calculate_simulation()
         if max < reward:
             max = reward
             x = i
     print("最小は", x, max)
 
 
-def actor_gcn_critic_gcn(log_file=None):
+def actor_gcn_critic_gcn(max_episodes=5000, test_name="test", log_file=False, save_pth=False):
     """Actor-Criticを行う．Actor,CriticはGCN
-    Actorの指定できるものは，ノード1とノード2であり，一つのエッジのみを選択できる．"""
-
-    max_episodes = 5000
-    test_name = "3"  # 実験名
+    Actorの指定できるものは，ノード1とノード2であり，一つのエッジのみを選択できる．
+    max_episodes:学習回数
+    test_name:保存ファイルの名前
+    log_file: Trueにすると，progress.txtに損失関数などの情報のログをとる．"""
 
     history = {}
     history['epoch'] = []
     history['result_efficiency'] = []
+    history['mean_efficiency'] = []  # a_meanの値の時のηの値を収納する
+    history['a'] = []
+    history['a_mean'] = []
+    history['a_sigma'] = []
+    history['advantage'] = []
+    history['critic_value'] = []
 
     log_dir = "confirm/step3/a_gcn_c_gcn_results/{}".format(test_name)
 
-    #assert not os.path.exists(log_dir), "already folder exists"
-    if log_file is not None:
-        log_file = os.path.join(log_dir, log_file)
+    assert not os.path.exists(log_dir), "already folder exists"
+    if log_file:
+        log_file = log_dir
     else:
         log_file = None
     os.makedirs(log_dir, exist_ok=True)
@@ -72,8 +79,8 @@ def actor_gcn_critic_gcn(log_file=None):
 
     device = torch.device('cpu')
 
-    actorNet = ActorNetwork_GCN(2, 1, 400, 400).to(device).double()
-    actorNet2 = ActorNetwork_GCN(3, 1, 400, 400).to(device).double()
+    actorNet = Select_node1_model(2, 1, 400, 400).to(device).double()
+    actorNet2 = Select_node2_model(400 + 2, 400).to(device).double()
     criticNet = CriticNetwork_GCN(2, 1, 400, 400).to(device).double()
     edgethickNet = Edgethick_Actor(2, 1, 400, 400).to(device).double()
     optimizer_actor = optim.Adam(actorNet.parameters(), lr=lr_actor)
@@ -82,8 +89,8 @@ def actor_gcn_critic_gcn(log_file=None):
     optimizer_critic = optim.Adam(
         criticNet.parameters(), lr=lr_critic, weight_decay=weight_decay)
 
-    for episode in range(max_episodes):
-        if log_file is not None:
+    for episode in tqdm(range(max_episodes)):
+        if log_file:
             with open(os.path.join(log_dir, "progress.txt"), mode='a') as f:
                 print('\nepoch:', episode, file=f)
         env.reset()
@@ -93,30 +100,27 @@ def actor_gcn_critic_gcn(log_file=None):
                 env, actorNet, actorNet2, criticNet, edgethickNet, device, log_dir=log_file)
 
             next_nodes_pos, _, done, _ = env.step(action)
-            reward = env.calculate_simulation(mode='force')
+            reward = env.calculate_simulation()
             criticNet.rewards.append(reward)
 
         loss = finish_episode(criticNet, actorNet, actorNet2, edgethickNet, optimizer_critic,
                               optimizer_actor, optimizer_actor2, optimizer_edgethick, gamma, log_dir=log_file)
 
-        history['epoch'].append(episode+1)
+        history['epoch'].append(episode + 1)
         history['result_efficiency'].append(reward)
-
         if episode % 100 == 0:
-            print("episode:{} total reward:{}".format(episode, reward))
+            if save_pth:
+                save_model(criticNet, edgethickNet, os.path.join(log_dir, "pth"), save_name=str(episode))
 
     env.close()
     plot_efficiency_history(history, os.path.join(
         log_dir, 'learning_effi_curve.png'))
 
+    return history
 
-def actor_gcn_critic_gcn_mean():
+
+def actor_gcn_critic_gcn_mean(test_num=5, max_episodes=5000, test_name="test", log_file=None):
     """Actor-Criticの５回実験したときの平均グラフを作成する関数"""
-
-    test_num = 5
-
-    max_episodes = 5000
-    test_name = "5times"  # 実験名
 
     log_dir = "confirm/step3/a_gcn_c_gcn_results/{}".format(test_name)
     assert not os.path.exists(log_dir), "already folder exists"
@@ -124,60 +128,7 @@ def actor_gcn_critic_gcn_mean():
 
     history = {}
     for i in range(test_num):
-        history["{}".format(i)] = {}
-        history["{}".format(i)]['epoch'] = []
-        history["{}".format(i)]['result_efficiency'] = []
-
-        node_pos, input_nodes, input_vectors,\
-            output_nodes, output_vectors, frozen_nodes,\
-            edges_indices, edges_thickness, frozen_nodes = easy_dev()
-        env = BarFemGym(node_pos, input_nodes, input_vectors,
-                        output_nodes, output_vectors, frozen_nodes,
-                        edges_indices, edges_thickness, frozen_nodes)
-        env.reset()
-
-        max_steps = 1
-        lr_actor = 1e-4
-        lr_critic = 1e-3
-        weight_decay = 1e-2
-        gamma = 0.99
-
-        device = torch.device('cpu')
-
-        actorNet = ActorNetwork_GCN(2, 1, 400, 400).to(device).double()
-        actorNet2 = ActorNetwork_GCN(3, 1, 400, 400).to(device).double()
-        criticNet = CriticNetwork_GCN(2, 1, 400, 400).to(device).double()
-        edgethickNet = Edgethick_Actor(2, 1, 400, 400).to(device).double()
-        optimizer_actor = optim.Adam(actorNet.parameters(), lr=lr_actor)
-        optimizer_actor2 = optim.Adam(actorNet2.parameters(), lr=lr_actor)
-        optimizer_edgethick = optim.Adam(
-            edgethickNet.parameters(), lr=lr_actor)
-        optimizer_critic = optim.Adam(
-            criticNet.parameters(), lr=lr_critic, weight_decay=weight_decay)
-
-        for episode in range(max_episodes):
-            env.reset()
-            nodes_pos, edges_indices, edges_thickness, node_adj = env.extract_node_edge_info()
-            for step in range(max_steps):
-                action = select_action_gcn_critic_gcn(
-                    env, actorNet, actorNet2, criticNet, edgethickNet, device)
-
-                next_nodes_pos, _, done, _ = env.step(action)
-                reward = env.calculate_simulation(mode='force')
-                criticNet.rewards.append(reward)
-
-            loss = finish_episode(criticNet, actorNet, actorNet2, edgethickNet, optimizer_critic,
-                                  optimizer_actor, optimizer_actor2, optimizer_edgethick, gamma)
-
-            history["{}".format(i)]['epoch'].append(episode+1)
-            history["{}".format(i)]['result_efficiency'].append(reward)
-
-            if episode % 100 == 0:
-                print("episode:{} total reward:{}".format(episode, reward))
-
-            env.close()
-            plot_efficiency_history(history["{}".format(i)], os.path.join(
-                log_dir, 'learning_effi_curve{}.png'.format(i)))
+        history["{}".format(i)] = actor_gcn_critic_gcn(max_episodes=max_episodes, test_name=os.path.join(test_name, str(i)), log_file=log_file)
 
     mean = np.stack([history["{}".format(i)]['result_efficiency']
                      for i in range(test_num)])
@@ -197,67 +148,9 @@ def actor_gcn_critic_gcn_mean():
         log_dir, 'mean_learning_effi_curve.png'))
 
 
-def actor_gcn_critic_gcn_confirm_loss():
-    """Actor-Criticを行う．Actor,CriticはGCN
-    Actorの指定できるものは，ノード1とノード2であり，一つのエッジのみを選択できる．"""
-
-    max_episodes = 5000
-    test_name = "確認"  # 実験名
-
-    history = {}
-    history['epoch'] = []
-    history['result_efficiency'] = []
-
-    log_dir = "confirm/step3/a_gcn_c_gcn_results/{}".format(test_name)
-    #assert not os.path.exists(log_dir), "already folder exists"
+def save_model(criticNet, edgethickNet, log_dir, save_name="Good"):
     os.makedirs(log_dir, exist_ok=True)
-
-    node_pos, input_nodes, input_vectors,\
-        output_nodes, output_vectors, frozen_nodes,\
-        edges_indices, edges_thickness, frozen_nodes = easy_dev()
-    env = BarFemGym(node_pos, input_nodes, input_vectors,
-                    output_nodes, output_vectors, frozen_nodes,
-                    edges_indices, edges_thickness, frozen_nodes)
-    env.reset()
-
-    max_steps = 1
-    lr_actor = 1e-4
-    lr_critic = 1e-3
-    weight_decay = 1e-2
-    gamma = 0.99
-
-    device = torch.device('cpu')
-
-    actorNet = ActorNetwork_GCN(2, 1, 400, 400).to(device).double()
-    actorNet2 = ActorNetwork_GCN(3, 1, 400, 400).to(device).double()
-    criticNet = CriticNetwork_GCN(2, 1, 400, 400).to(device).double()
-    edgethickNet = Edgethick_Actor(2, 1, 400, 400).to(device).double()
-    optimizer_actor = optim.Adam(actorNet.parameters(), lr=lr_actor)
-    optimizer_actor2 = optim.Adam(actorNet2.parameters(), lr=lr_actor)
-    optimizer_edgethick = optim.Adam(edgethickNet.parameters(), lr=lr_actor)
-    optimizer_critic = optim.Adam(
-        criticNet.parameters(), lr=lr_critic, weight_decay=weight_decay)
-
-    for episode in range(max_episodes):
-        env.reset()
-        nodes_pos, edges_indices, edges_thickness, node_adj = env.extract_node_edge_info()
-        for step in range(max_steps):
-            action = select_action_gcn_critic_gcn(
-                env, actorNet, actorNet2, criticNet, edgethickNet, device)
-
-            next_nodes_pos, _, done, _ = env.step(action)
-            reward = env.calculate_simulation(mode='force')
-            criticNet.rewards.append(reward)
-
-        loss = finish_episode(criticNet, actorNet, actorNet2, edgethickNet, optimizer_critic,
-                              optimizer_actor, optimizer_actor2, optimizer_edgethick, gamma)
-
-        history['epoch'].append(episode+1)
-        history['result_efficiency'].append(reward)
-
-        if episode % 100 == 0:
-            print("episode:{} total reward:{}".format(episode, reward))
-
-    env.close()
-    plot_efficiency_history(history, os.path.join(
-        log_dir, 'learning_effi_curve.png'))
+    torch.save(criticNet.state_dict(), os.path.join(
+        log_dir, '{}_criticNet.pth'.format(save_name)))
+    torch.save(edgethickNet.state_dict(), os.path.join(
+        log_dir, '{}_edgethickNet.pth'.format(save_name)))
